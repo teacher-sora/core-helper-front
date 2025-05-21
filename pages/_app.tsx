cec0c02b2b2063526ff09116bac509f9bcd6bc2e
teacher-sora <sora05153@gmail.com>
2025-05-21 23:30:53 +0900

    meta 설명 변경, meta의 image 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,12 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className="flex flex-column min-100vh">
       <Head>
         <title>코강 도우미</title>
-        <meta name="description" content="직업과 스킬을 선택 후 스크린샷을 올리면, 메이플스토리의 최적의 조합을 자동으로 분석해요. 복잡한 코강 고민을 도우미로 해결해보세요." />
+        <meta name="description" content="메이플스토리의 직업과 스킬을 선택하고 스크린샷을 업로드하면, 최적의 조합을 자동으로 분석해요. 복잡한 코강 고민을 도우미로 해결해보세요." />
         <meta name="author" content="메이플 코강 도우미" />
 
-        <meta property="og:title" content="메이플 코강 도우미 - 코강을 간편하게" />
-        <meta property="og:description" content="이미지를 업로드하여 시작해 보세요. 최적의 강화 코어 조합을 자동으로 알려드려요." />
-        <meta property="og:image" content="https://core-helper-back.onrender.com/static/images/logo.png" />
+        <meta property="og:title" content="메이플 코강 도우미 - 강화 코어 최적화 도구" />
+        <meta property="og:description" content="이미지 업로드로 코강을 해결해요. 최적의 강화 코어 조합을 자동으로 알려드려요." />
+        <meta property="og:image" content="https://hcrvm7kvolcgwdtv.public.blob.vercel-storage.com/core-gemstone" />
         <meta property="og:url" content="https://www.core-helper.site" />
         <meta property="og:type" content="website" />
       </Head>
