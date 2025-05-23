9b1b449c7c35a79af44f0c80f069e49aa84c734e
teacher-sora <sora05153@gmail.com>
2025-05-23 21:01:29 +0900

    find combinations를 클라이언트에서 계산하도록 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
   const [loadingMessage, setLoadingMessage] = useState<string>("");
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [result, setResult] = useState<ResultData>({});
+  const [resultMessage, setResultMessage] = useState<string>("");
 
   const [skillCombinations, setSkillCombinations] = useState<SkillData[][]>([]);
 
@@ -81,6 +81,7 @@ const Index = () => {
 
   const onSubmit = useCallback(async () => {
     setIsLoading(true);
+    setSkillCombinations([]);
     const formData = new FormData();
 
     formData.append("selected_job_class", selectedJobClass);
@@ -108,27 +109,33 @@ const Index = () => {
       const devServer = "http://127.0.0.1:8000/core-helper/";
       const liveServer = "https://core-helper-back.fly.dev/core-helper/";
 
-      const res = await fetch(liveServer, {
+      const res = await fetch(devServer, {
         method: "POST",
         body: formData,
       });
 
       if (!res.ok) {
-        setResult({ message: "분석 중 오류가 발생했어요.\n잠시 후 다시 시도해 주세요." })
+        setResultMessage("분석 중 오류가 발생했어요.\n잠시 후 다시 시도해 주세요.");
         return;
       }
 
       const data = await res.json();
 
       if (data["success"]) {
-        setResult({ combinations: data["combinations"] });
-        setSkillCombinations([]);
+        const combinations = findCoreCombinations(data["core_skill_names"]);
+
+        if (combinations.length > 0) {
+          setSkillCombinations(combinations);
+        }
+        else {
+          setResultMessage("현재 보유한 코어로는 조합이 어려워요.\n조금 더 코어를 모아주세요.");
+        }
       }
       else {
-        setResult({ message: data["message"] });
+        setResultMessage(data["message"]);
       }
     } catch (error) {
-      setResult({ message: "서버와 연결할 수 없어요.\n잠시 후 다시 시도해 주세요." });
+      setResultMessage("서버와 연결할 수 없어요.\n잠시 후 다시 시도해 주세요.");
     } finally {
       setIsLoading(false);
       clearAllTimeout([timer1, timer2, timer3, timer4]);
@@ -144,37 +151,149 @@ const Index = () => {
     });
   }
 
-  useEffect(() => {
-    setResult((prev) => {
-      if (Object.keys(prev).length === 0) {
-        return prev;
-      }
+  const findCoreCombinations = (array: string[][]) => {
+    const mainSkillIndices = getMainSkillIndices(array);
+    const allMainSkills = Object.keys(mainSkillIndices);
 
-      return {};
-    });
-  }, [selectedJob, selectedJobClass, selectedSkills, selectedImages]);
-  
-  useEffect(() => {
-    if (Object.keys(result).length === 0) return;
+    const minCaseCount = Math.ceil(2 * selectedSkills.length / 3);
+    const maxCaseCount = Math.min(2 * selectedSkills.length, allMainSkills.length) + 1;
 
-    const combinations = result["combinations"];
+    let validCoreCombinations: number[][] = [];
+    let minLength = maxCaseCount;
 
-    if (combinations) {
-      combinations.forEach((coreSkill) => {
-        let skillCombo: SkillData[] = []
+    for (let count = minCaseCount; count < maxCaseCount; count++) {
+      const mainSkillCombinations = generateCombinations(allMainSkills, count);
+      if (count >= minLength) break;
 
-        coreSkill.forEach((skill) => {
-          jobClassSkills[selectedJobClass].map((jobClassSkill) => {
-            if (jobClassSkill["skill"] === skill) {
-              skillCombo = [...skillCombo, jobClassSkill];
-            }
+      mainSkillCombinations.some((mainSkillcombination) => {
+        if (count >= minLength) return true;
+
+        let uniqueCoreCombinations: number[][] = [[]];
+        mainSkillcombination.forEach((mainSkill) => {
+          let newCoreCombination: number[][] = [];
+
+          uniqueCoreCombinations.forEach((uniqueCoreCombination) => {
+            mainSkillIndices[mainSkill].forEach((skillIndex) => {
+              newCoreCombination.push([...uniqueCoreCombination, skillIndex]);
+            });
           });
+
+          uniqueCoreCombinations = newCoreCombination;
         });
 
-        setSkillCombinations((prev) => [...prev, skillCombo]);
+        uniqueCoreCombinations.some((uniqueCoreCombination) => {
+          if (uniqueCoreCombination.length > minLength) return true;
+
+          let skillCounter: { [key: string]: number } = {};
+          uniqueCoreCombination.some((skillIndex) => {
+            array[skillIndex].forEach((skillCombination) => {
+              skillCounter[skillCombination] = (skillCounter[skillCombination] ?? 0) + 1;
+            });
+
+            if (selectedSkills.every((skill) => skill in skillCounter && skillCounter[skill] >= 2)) {
+              if (uniqueCoreCombination.length < minLength) {
+                minLength = uniqueCoreCombination.length;
+                validCoreCombinations = [uniqueCoreCombination];
+              }
+              else if (uniqueCoreCombination.length === minLength) {
+                validCoreCombinations.push(uniqueCoreCombination);
+              }
+            }
+          })
+        });
       });
     }
-  }, [result]);
+
+    if (validCoreCombinations.length === 0) return[];
+
+    const highestLevelCombination = findHighestLevelCombination(array, validCoreCombinations, mainSkillIndices);
+    const translatedCombination = translateToKorean(highestLevelCombination);
+
+    return translatedCombination;
+  }
+  
+  const getMainSkillIndices = (array: string[][]) => {
+    let indices: { [key: string]: number[] } = {};
+    array.forEach((coreSkill, index) => {
+      indices[coreSkill[0]] = indices[coreSkill[0]] ? [...indices[coreSkill[0]], index] : [index];
+    });
+
+    return indices;
+  }
+
+  const generateCombinations = (array: string[], r: number) => {
+    const results: string[][] = [];
+
+    if (r === 1) return array.map((value) => [value]);
+
+    array.forEach((value, index, original) => {
+      const rest = original.slice(index + 1);
+      const combinations = generateCombinations(rest, r - 1);
+      const attached = combinations.map(combination => [value, ...combination]);
+      results.push(...attached);
+    });
+
+    return results;
+  }
+
+  const findHighestLevelCombination = (array: string[][], validCoreCombinations: number[][], mainSkillIndices: { [key: string]: number[] }) => {
+    const expPerCore = 50;
+    const levelExpRequirements = [0, 55, 125, 210, 310, 425, 555, 700, 860, 1035, 1225, 1430, 1650, 1885, 2135, 2400, 2680, 2975, 3285, 3610, 3950, 4305, 4675, 5060, 5460];
+
+    let totalCoreLevels: number[] = [];
+    let validCoreNameCombinations: string[][][] = [];
+
+    validCoreCombinations.forEach((validCoreCombination) => {
+      let totalCoreLevel = 0;
+
+      const validCoreNameCombination = validCoreCombination.map(coreSkillIndex => array[coreSkillIndex]);
+      const mainSkillNames = validCoreNameCombination.map((skillName) => skillName[0]);
+
+      mainSkillNames.forEach((skillName) => {
+        const count = mainSkillIndices[skillName].length;
+        const coreExp = count * expPerCore;
+
+        levelExpRequirements.some((expRequirement, index) => {
+          if (coreExp <= expRequirement) {
+            totalCoreLevel += index;
+            return true;
+          }
+        });
+      });
+
+      totalCoreLevels.push(totalCoreLevel);
+      validCoreNameCombinations.push(validCoreNameCombination);
+    });
+
+    const maxLevel = Math.max(...totalCoreLevels);
+    const maxLevelIndex = totalCoreLevels.indexOf(maxLevel);
+
+    return validCoreNameCombinations[maxLevelIndex];
+  }
+
+  const translateToKorean = (array: string[][]) => {
+    let translatedArray: SkillData[][] = [];
+
+    array.forEach((coreSkill) => {
+      let skillCombination: SkillData[] = [];
+
+      coreSkill.forEach((skill) => {
+        jobClassSkills[selectedJobClass].map((jobClassSkill) => {
+          if (jobClassSkill["skill"] === skill) {
+            skillCombination = [...skillCombination, jobClassSkill];
+          }
+        });
+      });
+
+      translatedArray = [...translatedArray, skillCombination];
+    });
+
+    return translatedArray;
+  }
+
+  useEffect(() => {
+    setResultMessage("");
+  }, [selectedJob, selectedJobClass, selectedSkills, selectedImages]);
 
   return (
     <div className="relative flex flex-column justify-center align-center flex-1">
@@ -259,11 +378,11 @@ const Index = () => {
               }
             </button>
             {
-              (!isLoading && result["message"]) ? (
+              (!isLoading && resultMessage.length > 0) ? (
                 <div className="flex justify-center">
-                  <p className="text-center space-pre font-14 white">{ result["message"] }</p>
+                  <p className="text-center space-pre font-14 white">{ resultMessage }</p>
                 </div>
-              ) : (!isLoading && result["combinations"]) ? (
+              ) : (!isLoading && skillCombinations.length > 0) ? (
                 <div className={`grid gap-15 ${styles.result}`}>
                   {
                     skillCombinations.map((skillCombination, idx) => (
